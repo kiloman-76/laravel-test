9a5b4cfc5e60c54fcd5d4745b2dbdfd016bce73b
Василий <medvedko_vasilii@mail.ru>
2019-07-04 17:32:20 +0700

    Добавление начального функционала админки, создание страницы списка категорий

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,16 @@ Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function (){
    Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
-//Route::resource('rest', 'RestTestController')->names('restTest');
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['namespace' => 'Blog\Admin', 'prefix' => 'admin/blog'], function(){
+   $methods = ['index', 'edit', 'store', 'update', 'create'];
+   Route::resource('categories', 'CategoryController')
+       ->only($methods)
+       ->names('blog.admin.categories');
+});
+
+//Route::resource('rest', 'RestTestController')->names('restTest');
