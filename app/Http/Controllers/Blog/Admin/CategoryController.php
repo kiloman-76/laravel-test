f571ef81b1ba6fd0728e76adf648340e8824ee19
Василий <medvedko_vasilii@mail.ru>
2019-07-05 14:02:12 +0700

    Создание страницы изменения категории

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -52,7 +52,7 @@ class CategoryController extends BaseController
         $item = BlogCategory::findOrFail($id);
         $categoryList = BlogCategory::all();
 
-        return view('blog.admin.category.index', compact('item', 'categoryList'));
+        return view('blog.admin.category.edit', compact('item', 'categoryList'));
 
     }
 
